b793e76cc8dc55c309e77e06991c2ac62bd70717
Ankit Goel <ankit1ank@gmail.com>
2017-11-22 00:46:11 +0530

    Fix installation script

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-curl https://github.com/ankit1ank/virtualswift/blob/master/virtualswift > /usr/local/bin/virtualswift
+curl https://raw.githubusercontent.com/ankit1ank/virtualswift/master/virtualswift > /usr/local/bin/virtualswift
 chmod +x /usr/local/bin/virtualswift
